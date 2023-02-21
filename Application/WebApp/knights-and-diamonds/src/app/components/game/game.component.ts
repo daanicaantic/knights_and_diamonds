a5fd0c94475559e8cc248f19f4f48d95e0e13ad2
Spasa98 <dusanspasicspasa@gmail.com>
2023-02-21 12:38:48 +0100

    rps

--- Application/WebApp/knights-and-diamonds/src/app/components/game/game.component.ts
+++ Application/WebApp/knights-and-diamonds/src/app/components/game/game.component.ts
@@ -13,22 +13,51 @@ export class GameComponent implements OnInit {
   user1: any;
   user2: any;
   userID = this.authService?.userValue?.id;
+  progressValue!:number;
+  loadingBar:any;
+  gameStarted:Boolean=false;
+  timer:any;
+  timerCountDown:any;
+  timerContainerWidth:any;
 
   constructor(private route: ActivatedRoute,
     private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.gameID = this.route.snapshot.params['gameID']
-    console.log('ID GAME',this.gameID)
+    this.progressValue = 0;
+    let progressEndValue = 100;
+    let speed = 50;
+    this.timer = 30;
 
-    const ons$ = timer(5000,1000)
-    ons$.subscribe((d)=>{
-      console.log(d)
-      if(d==10)
-      {
-        
+
+    let progress = setInterval(() => {
+
+      this.progressValue++;
+      this.loadingBar='conic-gradient(red '+this.progressValue*3.6+'deg,orange '+this.progressValue*3.6+ 'deg)';
+
+      if (this.progressValue == progressEndValue) {
+        clearInterval(progress);
+        this.gameStarted=true;
+        this.progressValue=0;
+        progress=setInterval(()=>
+        {
+          console.log(this.timer,"caocao")
+          this.timer--;
+          this.timerCountDown=this.timer*3.33333333333 + "%"
+          console.log("odje",this.timerCountDown);
+          if(this.timer===this.progressValue)
+          {
+            console.log(this.timer,"caocao")
+            clearInterval(progress);
+          }
+
+        },1000)
       }
-    })
+      speed=50;
+    }, speed);
   }
-
+  ChooseMove(move:String)
+  {
+    console.log(move);
+  }
 }
